fcdb8a92a379b44c59de0ce24ca2ca410c304092
えんがわ釣り <engawatsuri@icloud.com>
2026-06-28 20:39:29 +0900

    バグ修正

--- month.js
+++ month.js
@@ -2,99 +2,99 @@ function init_month() {
     switch (page) {
         case 'apr':
             msg.textContent = "みなさん。4月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'may':
             msg.textContent = "みなさん。5月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'jun':
             msg.textContent = "みなさん。6月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'jul':
             msg.textContent = "みなさん。7月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'aug':
             msg.textContent = "みなさん。8月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'sep':
             msg.textContent = "みなさん。9月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'oct':
             msg.textContent = "みなさん。10月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'nov':
             msg.textContent = "みなさん。11月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'dec':
             msg.textContent = "みなさん。12月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'jan':
             msg.textContent = "みなさん。1月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'feb':
             msg.textContent = "みなさん。2月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         case 'mar':
             msg.textContent = "みなさん。3月です";
-            back.addEventListener('click', () => {
-            });
-            next.addEventListener('click', () => {
+            back.onclick = () => {
+            };
+            next.onclick = () => {
                 changePage('start');
-            });
+            };
             break;
         default:
             return -1;
